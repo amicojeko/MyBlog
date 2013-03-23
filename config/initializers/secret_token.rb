925861b58df8e46e7630d865305cf45b351a5404
stefano@interact.it <Jeko@JekBook-Air.local>
2013-03-23 18:47:09 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '1319a2a27d6f2b61910d4b860a8b65ee4408f02ede280e92c94f47a2b61212e5b5f9789ad260523342feb11e9ac81667e12b966a0ebe306dd88c45b64bd66d88'
